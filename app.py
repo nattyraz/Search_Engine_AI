a4cd22d3265fa5f2d21323c5ba897a32df843e68
ROFOX <rgfogliami@gmail.com>
2023-08-16 16:04:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,25 +3,24 @@ import pandas as pd
 from transformers import BertTokenizer, BertModel
 import torch
 import pinecone
-import os
 
 PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
 PINECONE_ENV = st.secrets["PINECONE_ENVIRONMENT"]
 
-# Initialisation de Pinecone juste après avoir récupéré la clé API
 pinecone.init(
     api_key=PINECONE_API_KEY,
     environment=PINECONE_ENV
 )
 
-
 INDEX_NAME = "semantic-search-index"
+index = pinecone.Index(INDEX_NAME)
+
 if INDEX_NAME not in pinecone.list_indexes():
     pinecone.create_index(index_name=INDEX_NAME, dimension=768)
 
-
-tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
-model = BertModel.from_pretrained('bert-base-multilingual-cased')
+with st.spinner('Chargement du modèle et du tokenizer...'):
+    tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
+    model = BertModel.from_pretrained('bert-base-multilingual-cased')
 
 def encode_text(text):
     tokens = tokenizer(text, return_tensors='pt', truncation=True, max_length=512, padding='max_length')
@@ -32,23 +31,21 @@ def encode_text(text):
 def extract_document_from_row(row):
     return " ".join(row.dropna().astype(str).tolist())
 
-# Interface Streamlit
 st.title("Moteur de recherche sémantique")
 
 uploaded_file = st.file_uploader("Téléchargez votre fichier Excel", type=["xlsx"])
 
 if uploaded_file:
-    df = pd.read_excel(uploaded_file)
-    documents = df.apply(extract_document_from_row, axis=1).tolist()
-    document_vectors = {str(i): encode_text(doc) for i, doc in enumerate(documents)}
-    
-    # Insérer les vecteurs dans Pinecone
-    pinecone.upsert(vectors=document_vectors, index_name=INDEX_NAME)
+    with st.spinner('Traitement du fichier...'):
+        df = pd.read_excel(uploaded_file)
+        documents = df.apply(extract_document_from_row, axis=1).tolist()
+        document_vectors = [{'id': str(i), 'values': encode_text(doc).tolist()} for i, doc in enumerate(documents)]
+        
+        upsert_response = index.upsert(vectors=document_vectors)
     st.success("Fichier téléchargé et indexé avec succès!")
 
     query = st.text_input("Entrez votre requête de recherche :")
     if query:
         query_vector = encode_text(query)
-        # Utiliser Pinecone pour la recherche
-        results = pinecone.query(queries=[query_vector], top_k=5, index_name=INDEX_NAME)
+        results = index.query(queries=[query_vector.tolist()], top_k=5)
         st.write("Résultats :", results)
